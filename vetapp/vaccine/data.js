175ea274338a1b2aa56c85a175c8561c4c76fdea
vydnquang <vyducnhatquang@gmail.com>
2025-08-31 00:12:26 +0700

    Update data.js
    thêm vaccine mèo

--- vetapp/vaccine/data.js
+++ vetapp/vaccine/data.js
@@ -1,9 +1,9 @@
 // data.js
 
-let csvData = `tên thú cưng,tên chủ,địa chỉ,số điện thoại,ngày tiêm phòng vaccine 7 bệnh,ngày tiêm phòng dại
-Buddy,Nguyễn Văn A,123 Nguyễn Huệ,0912345678,2024-08-01,2024-08-01
-Lucky,Trần Thị B,456 Lê Lợi,0987654321,2025-01-15,2025-01-15
-Luxy,Trần Thị N,56 Lê Lợi,0997654321,2025-08-25,2025-08-15
-Nuxy,Trần Thị D,58 Lê Lợi,0997654331,2024-08-30,2024-08-25
-Lim,Trần V D,5558 Lê Lợi,0997644331,2024-08-31,2024-09-02
-Milo,Lê Văn C,789 Hoàng Diệu,0909090909,2023-05-20,2023-05-20`;
+let csvData = `tên thú cưng,tên chủ,địa chỉ,số điện thoại,loại vật nuôi,ngày tiêm phòng dại,ngày tiêm phòng vaccine 7 bệnh,ngày tiêm phòng vaccine 4 bệnh
+Buddy,Nguyễn Văn A,123 Nguyễn Huệ,0912345678,chó,2024-08-01,2024-09-01,,
+Lucky,Trần Thị B,456 Lê Lợi,0987654321,mèo,2025-01-15,,2025-01-15
+Luxy,Trần Thị N,56 Lê Lợi,0997654321,chó,2025-08-25,2025-08-15,,
+Nuxy,Trần Thị D,58 Lê Lợi,0997654331,chó,2024-08-30,2024-08-25,,
+Lim,Trần V D,5558 Lê Lợi,0997644331,mèo,2024-08-31,,2024-09-02
+Milo,Lê Văn C,789 Hoàng Diệu,0909090909,mèo,2023-05-20,,2023-05-20`;
